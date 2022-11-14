54749b25693bad7bfb4207e8eae642d8e16f4bb7
jon-robbins <jonathan.robbins@bse.eu>
2022-11-14 10:21:12 +0100

    Update hw5.py

--- hw5.py
+++ hw5.py
@@ -11,6 +11,15 @@
 # "Undefined instruction for color: <light>" 
 # where <light> is the value of the parameter light.
 #
+def car_at_light(light):
+    if light == 'red':
+        return 'stop'
+    elif light == 'yellow':
+        return 'wait'
+    elif light == 'green':
+        return 'go'
+    else:
+        raise Exception(f'Undefined instruction for color {light}')
 
 # 2)
 # Create a function named "safe_subtract" that
@@ -21,6 +30,15 @@
 # If there is any other reason why it fails show the problem 
 # 
 
+def safe_subtract(x,y):
+    try:
+        return x - y
+    except TypeError:
+        return None
+    else:
+        return "Could not be subtracted due to type"
+
+
 # 3)
 # Imagine you have a dictionary with the attributes of a person
 # {'name': 'John', 'last_name': 'Doe', 'birth': 1987}
@@ -30,12 +48,38 @@
 # Name the first function "retrieve_age_eafp" and follow EAFP
 # Name the second function "retrieve_age_lbyl" and follow lbyl
 
+def retrieve_age_eafp(x: dict):
+    try:
+        return 2022 - x['birth']
+    except KeyError:
+        return 'error'
+
+
+def retrieve_age_lbyl(x: dict):
+    if 'birth' in x.keys():
+        return 2022 - x['birth']
+    else:
+        print(f'failed for dict {x}')
+        pass
+
+
 # 4)
 # Imagine you have a file named data.csv. 
 # Create a function called "read_data" that reads the file
 # making sure to use to handle the fact 
 # that it might not exist. 
 #
+
+import csv
+
+
+def read_data(filepath):
+    try:
+        with open(filepath, 'r') as data:
+            for line in csv.reader(data):
+                return line
+    except FileNotFoundError:
+        return 'File Not Found'
 
 
 # 5) Squash some bugs! 
@@ -46,26 +90,52 @@
 total_double_sum = 0
 for elem in [10, 5, 2]:
     double = elem * 2
+
     total_double_sum += elem
+# total_double_sum is only counted with the values of elem. The easiest way to get the double value of all elements in a list combined would be just to sum the list without iteration, like so:
+
+input_list = [10, 5, 2]
+total_double_sum = sum(input_list) * 2
+# If for some reason you want to iterate, you could use the code below
+
+total_double_sum = 0
+for elem in [10, 5, 2]:
+    double = elem * 2
+    total_double_sum += double
 
 ### (b)
 strings = ''
 for string in ['I', 'am', 'Groot']:
     strings = string+"_"+string
+# Looks like you're trying to combine elements in a list into one string.
+# #What this code actually does is iterates over a list, and returns the last string as
+#"Groot_Groot" because you iterate without saving each iteration anywhere.
+# Because strings is defined as one object, not an iterable, you can't iterate over it.
+# Instead join like below
+str_concat = '_'.join(strings)
 
 ### (c) Careful!
 j=10
 while j > 0:
+   j += 1
+# This is just going to keep adding from 10 until infinity, because the while condition
+# is set to j>0, and j will always be >0 while you keep adding one to 10. I don't know what this
+# iteration is trying to achieve, but if you're trying to count from 1 to 10, just change it like below
+j=0
+while j < 10:
    j += 1
 
 ### (d)
 productory = 0
 for elem in [1, 5, 25]:
     productory *= elem
+#Anything * 0 is 0. Start from 1 instead.
+productory = 1
+for elem in [1, 5, 25]:
+    productory *= elem
 
-
-################################################
 ##### Try to use map and reduce in the next 3 exercises
+################################################
 # 6)
 # Create a function called "count_simba" that counts
 # the number of times that Simba appears in a list of
@@ -73,6 +143,16 @@ for elem in [1, 5, 25]:
 # ["Simba and Nala are lions.", "I laugh in the face of danger.",
 #  "Hakuna matata", "Timon, Pumba and Simba are friends, but Simba could eat the other two."] 
 #
+strings = ["Simba and Nala are lions.", "I laugh in the face of danger.",
+  "Hakuna matata", "Timon, Pumba and Simba are friends, but Simba could eat the other two."]
+
+count = 0
+for i in strings:
+    if 'Simba' in i:
+        count += 1
+
+count2 = len(list(filter(lambda x: 'Simba' in x, strings)))
+#count2 is less readable, so if efficiency isn't a problem the for loop is better.
 
 # 7)
 # Create a function called "get_day_month_year" that takes 
